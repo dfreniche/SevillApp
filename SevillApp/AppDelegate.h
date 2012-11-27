9e1e514b71f898fe6bc630be2351c12d09ecf6bf
dfreniche <dfreniche@Tesla.local>
2012-11-27 05:19:54 +0100

    1st version

---
+++ SevillApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SevillApp
+//
+//  Created by Diego Freniche Brito on 27/11/12.
+//  Copyright (c) 2012 Diego Freniche Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SevillAppTests/SevillAppTests.m
@@ -0,0 +1,32 @@
+//
+//  SevillAppTests.m
+//  SevillAppTests
+//
+//  Created by Diego Freniche Brito on 27/11/12.
+//  Copyright (c) 2012 Diego Freniche Brito. All rights reserved.
+//
+
+#import "SevillAppTests.h"
+
+@implementation SevillAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SevillAppTests");
+}
+
+@end
